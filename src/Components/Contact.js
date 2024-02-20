5dc08d7e83652143586cc1898bf09593c46c0a55
Saimahesh-782 <saimahesh645@gmail.com>
2024-02-20 22:13:49 +0530

    commit

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -7,16 +7,22 @@ const Contact = () => {
     <div className="contact">
       
     <img src={Contactimage} alt="contact"  className='Aboutmainimage' />
-   
-    <div className='Form-container'>
     <h2>Contact Me</h2>
-      <div className="contact-left">
-        <h2>Reach me via</h2>
-        <h3>Feel free to message me</h3>
-      </div>
+    <div className='Form-container'>
+    
+       <div className="contact-left">
+         <h2>Reach me via</h2>
+         <h3>Feel free to message me or inquire about hiring team for me </h3>
+          <h2>Name</h2>
+          <h2>Sai Mahesh</h2>
+          <h2>Email</h2>
+          <h2>saimahesh645@gmail.com</h2>
+       </div>
+
       <div className="contact-right">
        
       <form action="" >
+        <h2> I Welcome Your messages</h2>
       <input type="text" placeholder='Enter Your Name' />
       <input type="email" placeholder='Enter Your email' />
       <input type="tel" placeholder='Enter Your Mobile Number' />
