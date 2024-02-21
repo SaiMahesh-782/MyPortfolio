fdc74231d0024721b8f286a9f5f5647e54056bde
Sai Mahesh <82072824+SaiMahesh-782@users.noreply.github.com>
2024-02-21 09:59:42 +0530

    Update About.js

--- src/Components/About.js
+++ src/Components/About.js
@@ -57,7 +57,7 @@ const About = () => {
    <div className="About-me">
     <div className="left-align">
     <p>
-      Hi, I am Sai Mahesh, a Software Engineer with 2 years of experience in building tech products. I have experience in working in multiple domains, including henamehcare projects, but not limited to E-commerce.
+      Hi, I am Sai Mahesh, a Software Engineer with 2 years of experience in building tech products. I have experience in working in multiple domains, including healthcare projects, but not limited to E-commerce.
       Experienced in React.js,  Node.js, Express.js, Redux, JavaScript, MySQL, HTML5, CSS, Tailwind CSS,  and Basic knowledge in AWS.
       Highly skilled in building complex, large-scale, and performant web applications. I am a passionate engineer who strives to build pixel-perfect designs and has a strong aptitude for learning new technologies.
     </p>
